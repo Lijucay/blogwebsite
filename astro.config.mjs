084505bec4b74b11419bf298e0a65d7dd10fd440
Lijucay <lijucayy@proton.me>
2025-06-17 21:47:30 +0200

    Adding rehype for customization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,30 @@
-// @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import mdx from '@astrojs/mdx';
+import rehypePrettyCode from "rehype-pretty-code";
 // https://astro.build/config
 export default defineConfig({
     vite: {
+        experimental: {
+            inspector: true
+        },
         plugins: [
             tailwindcss()
         ]
     },
+    markdown: {
+        syntaxHighlight: false,
+        rehypePlugins: [
+            [
+                rehypePrettyCode,
+                {
+                    theme: 'material-theme-ocean',
+                    keepBackground: false
+                }
+            ]
+        ],
+    },
     output: "static",
-    integrations: [react()]
+    integrations: [tailwindcss(), react(), mdx()]
 });
