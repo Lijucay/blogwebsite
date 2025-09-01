3457ca078a3caa3e68ec4522f4bde0314f2e4846
Lijucay <lijucayy@proton.me>
2025-09-01 14:06:04 +0200

    Blog collections

---
+++ src/content.config.ts
@@ -0,0 +1,41 @@
+import { defineCollection } from "astro:content";
+import { glob, file } from "astro/loaders";
+import {z} from "zod";
+
+const qwotableEntries = defineCollection({
+    loader: glob({
+        pattern: "**/*.md",
+        base: "./src/data/qwotable"
+    }),
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+        image: z.object({
+            url: z.string(),
+            alt: z.string()
+        }),
+        author: z.string(),
+        tags: z.array(z.string()),
+    })
+})
+
+const bitsAndBeyondEntries = defineCollection({
+    loader: glob({
+        pattern: "**/*.md",
+        base: "./src/data/bits_and_beyond"
+    }),
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date(),
+        description: z.string(),
+        image: z.object({
+            url: z.string(),
+            alt: z.string()
+        }),
+        author: z.string(),
+        tags: z.array(z.string()),
+    })
+})
+
+export const collections = { qwotableEntries, bitsAndBeyondEntries }
